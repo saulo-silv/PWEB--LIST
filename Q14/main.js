846e1ec7d9718e089376678ceaa3d0767c71a0c3
saulo-silv <vitorgabrielsaulo@gmail.com>
2022-10-13 19:21:23 -0300

    segundo commit

--- Q14/main.js
+++ Q14/main.js
@@ -2,6 +2,6 @@ let tempo_fumante=parseInt(prompt("quantos anos de fumante:"));
 let num_cigarros= parseInt(prompt("quantos cigarros fumados por dia:"));
 let valor_carteira= parseInt(prompt("valor da carteira de cigarro:"));
 
-let gasto_dia = num_cigarros*valor_carteira;
+let gasto_dia = num_cigarros/valor_carteira;
 let gasto_total = (tempo_fumante*365)*gasto_dia;
 document.write("valor gasto: "+ gasto_total+ " reais")
